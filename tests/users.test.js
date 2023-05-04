3203a54f9e078e20a95da8d52f0d6bbbf061af41
Lewis Smith <lewislbssmith212@gmail.com>
2023-05-04 20:22:43 +0100

    validation tests added

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,4 +1,4 @@
-const { expect } = require('chai');
+const { expect, assert } = require('chai');
 const request = require('supertest');
 const { Users } = require('../src/models');
 const app = require('../src/app');
@@ -17,7 +17,7 @@ describe('/users', () => {
           first_name: 'John',
           last_name: 'Doe',
           email_address: 'JohnDoe@gmail.com',
-          password: 'secretpassword',
+          password: 'Secretpassword1#',
         });
 
         const newUserRecord = await Users.findByPk(response.body.id, {
@@ -28,11 +28,11 @@ describe('/users', () => {
         expect(response.body.first_name).to.equal('John');
         expect(response.body.last_name).to.equal('Doe');
         expect(response.body.email_address).to.equal('JohnDoe@gmail.com');
-        expect(response.body.password).to.equal('secretpassword');
+        expect(response.body.password).to.equal('Secretpassword1#');
         expect(newUserRecord.first_name).to.equal('John');
         expect(newUserRecord.last_name).to.equal('Doe');
         expect(newUserRecord.email_address).to.equal('JohnDoe@gmail.com');
-        expect(newUserRecord.password).to.equal('secretpassword');
+        expect(newUserRecord.password).to.equal('Secretpassword1#');
       });
     });
   });
@@ -45,19 +45,19 @@ describe('/users', () => {
           first_name: 'John',
           last_name: 'Doe',
           email_address: 'JohnDoe@gmail.com',
-          password: 'secretpassword',
+          password: 'Secretpassword1#',
         }),
         Users.create({
           first_name: 'Jane',
           last_name: 'Doe',
           email_address: 'JaneDoe@gmail.com',
-          password: 'secretpassword1',
+          password: 'Secretpassword1#',
         }),
         Users.create({
           first_name: 'Jimmy',
           last_name: 'Hendrix',
           email_address: 'JimmyHendrix@gmail.com',
-          password: 'secretpassword2',
+          password: 'Secretpassword1#',
         }),
       ]);
     });
@@ -126,16 +126,96 @@ describe('/users', () => {
         expect(updatedUserRecord.email_address).to.equal('anewemail@gmail.com');
       });
 
-
       it('returns a 404 if the user does not exist', async () => {
         const response = await request(app)
           .patch('/users/12345')
           .send({ email: 'anewemail@gmail.com' });
-  
+
         expect(response.status).to.equal(404);
         expect(response.body.message).to.equal('User not found');
       });
     });
+  });
+});
 
+describe('/users validations', () => {
+  it('should not allow empty first_name', async () => {
+    try {
+      await Users.create({
+        first_name: '',
+        last_name: 'Doe',
+        email_address: 'john.doe@example.com',
+        password: 'Abc@1234',
+      });
+    } catch (error) {
+      assert.strictEqual(error.name, 'SequelizeValidationError');
+      assert.strictEqual(
+        error.errors[0].message,
+        'First name must only contain letters and spaces'
+      );
+      assert.strictEqual(
+        error.errors[1].message,
+        'First name length must be between 1 and 50 characters'
+      );
+    }
+  });
+
+  it('should not allow empty last_name', async () => {
+    try {
+      await Users.create({
+        first_name: 'John',
+        last_name: '',
+        email_address: 'john.doe@example.com',
+        password: 'Abc@1234',
+      });
+    } catch (error) {
+      assert.strictEqual(error.name, 'SequelizeValidationError');
+      assert.strictEqual(
+        error.errors[0].message,
+        'Last name must only contain letters and spaces'
+      );
+      assert.strictEqual(
+        error.errors[1].message,
+        'Last name length must be between 1 and 50 characters'
+      );
+    }
+  });
+
+  it('should ensure email is of correct type', async () => {
+    try {
+      await Users.create({
+        first_name: 'John',
+        last_name: 'Doe',
+        email_address: 'john.doeexample.com',
+        password: 'Abc@1234',
+      });
+    } catch (error) {
+      assert.strictEqual(error.name, 'SequelizeValidationError');
+      assert.strictEqual(
+        error.errors[0].message,
+        'Invalid email address format'
+      );
+    }
+  });
+
+  it('should ensure password is at least 6 characters long, and has one uppercase letter, number, and special character', async () => {
+    try {
+      await Users.create({
+        first_name: 'John',
+        last_name: 'Doe',
+        email_address: 'john.doe@example.com',
+        password: 'abc34',
+      });
+    } catch (error) {
+      assert.strictEqual(error.name, 'SequelizeValidationError');
+      assert.strictEqual(
+        error.errors[0].message,
+        'Password must be at least 6 characters long'
+      );
+      assert.strictEqual(
+        error.errors[1].message,
+        'Password must contain at least one uppercase letter and one special character'
+      );
+    }
   });
 });
