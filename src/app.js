57bbaa0ab16f266458b7cbf4d3a8d4eba78f8d20
Tom Wilkinson <thomasmarkwilkinson@gmail.com>
2023-05-11 20:21:48 +0100

    server added for cryto API

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const axios = require('axios');
 const cors = require('cors');
 const usersRouter = require('./routes/users');
 const tradesRouter = require('./routes/trades');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
@@ -9,4 +11,19 @@ app.use(express.json());
 app.use('/users', usersRouter);
 app.use('/tradehistory', tradesRouter);
 
+const api = axios.create({
+  method: 'GET',
+  baseURL: 'https://pro-api.coinmarketcap.com',
+  headers: {
+    'X-CMC_PRO_API_KEY': process.env.COINMARKETCAP_API_KEY,
+    Accept: 'application/json',
+    'Accept-Encoding': 'deflate, gzip',
+  },
+});
+app.get('/api', (req, res) => {
+  api('/v1/cryptocurrency/listings/latest')
+    .then((response) => response.data)
+    .then((value) => res.json(value.data))
+    .catch((err) => console.log(err));
+});
 module.exports = app;
